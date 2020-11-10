4ca6a8f1be2c4dd134371d16066e5045b641d616
Horntobewild <36193283+Horntobewild@users.noreply.github.com>
2020-11-10 21:03:11 +0100

    fix of parseArticle function

--- src/app/services/parser.service.ts
+++ src/app/services/parser.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -7,14 +7,14 @@ import { BehaviorSubject } from 'rxjs';
 })
 
 
-
 export class ParserService {
 
   private parserSubject = new BehaviorSubject<any>('no content parsed yet');
   currentParsedArticle = this.parserSubject.asObservable();
 
+
   diffbotToken:string = "42df55145e1270c1462a2f994e009c6a"
-  getAdress:string = "https://api.diffbot.com/v3/article?"
+  getAdress:string = "https://api.diffbot.com/v3/article"
   response:any
 
   constructor(
@@ -23,13 +23,11 @@ export class ParserService {
 
   ) { }
 
-
-  parseArticle(url:string) {
-    console.log("query received to: ",url)
-    this.http.get(this.getAdress+"token="+this.diffbotToken+"&url="+url).subscribe(
-      arg => this.parserSubject.next(arg['objects'][0])
-      )
-    }
+  
+  parseARticle(url:string){
+    const params = new HttpParams({ fromObject: { token: this.diffbotToken, url}});
+    this.http.get(this.getAdress, {params});
+  }
 
 
   clearResults(){
